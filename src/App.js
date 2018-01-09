d7271f8db8f2356513f78c8e805a64291c7520fc
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2018-01-09 09:53:50 -0800

    Player can now move up in the world. Hooray for upward mobility

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import tileMap from './tiles/map';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tileMap: this.getTileMap()
+    }
+  }
 
   componentDidMount(){
     document.addEventListener('keydown', this.handleKeyPress, false);
@@ -37,6 +43,25 @@ class App extends Component {
   handleKeyPress = (event) => {
     if(event.key === 'w'){
       console.log('w');
+      // move @ symbol up in tileMap
+      var tiles = this.state.tileMap;
+      // iterate through tilemap and move the position of the player (2) up.
+      
+      // we should really keep a cache of the player's position
+      for(var i = 0; i < tiles.length; i++) {
+        for(var j = 0; j < tiles[i].length; j++) {
+           if(tiles[i][j] === 2) {
+             tiles[i][j] = 0;
+             tiles[i-1][j] = 2;
+             this.setState({
+               tileMap: tiles
+             });
+           }
+        }
+      }
+
+
+
     } else if(event.key === 's') {
       console.log('s');
     } else if(event.key === 'a') {
@@ -47,7 +72,7 @@ class App extends Component {
   }
 
   render() {
-    var tileMap = this.getTileMap();
+    var tileMap = this.state.tileMap;
     var renderElement = this.transformElement;
     var handleKeyPress = this.handleKeyPress;
     return (
